0b468db4030666f814b245165d72ce1e17d970b2
Gustavo Mazui <gustavopmazui18@gmail.com>
2024-09-19 11:48:24 -0300

    Add files via upload

---
+++ Exercicios Slide Strings Gustavo Mazui.js
@@ -0,0 +1,58 @@
+
+/////////////////////////////////EXERCICIO 1 
+
+
+/* const nome = prompt("Qual é o seu nome?")
+let cor = prompt("Qual é sua cor favorita")
+
+console.log("A cor de favorita de " + nome + " é " + cor) */
+
+
+
+
+/* const nome = prompt("Qual é o seu nome?")
+let cor = prompt("Qual é sua cor favorita")
+
+console.log(`A cor de favorita de ${nome} é ${cor}` ) */
+
+
+
+
+/////////////////////////////////EXERCICIO 2 
+
+/* let frase = prompt("Escreva uma frase")
+const fraseMaiuscula = frase.toUpperCase (frase)  
+const linguaI = frase.replaceAll ("o","i")
+
+console.log(`frase normal: ${frase}`)
+console.log(`em maiusculo: ${fraseMaiuscula}`) 
+console.log(`lingua do I: ${linguaI}`)
+console.log(`tamanho da frase: ${frase.length}`) */ 
+
+
+
+/////////////////////////////////EXERCICIO 3
+
+/* let numero = prompt ("Insira um numero de 0 a 5:")
+
+const listaDogs = ["Poodle", "Beagle", "Dachshund", "Border Collie", "Golden Retriever", "Shih Tzu"]
+console.log(listaDogs[numero]) */ 
+
+
+
+
+/////////////////////////////////EXERCICIO 4
+
+/* let numeros = [1, 2, 3, 4, 5, 6] /*array criado
+console.log(numeros.length)
+
+numeros.push(7)
+console.log (numeros) /* numero 7 Add 
+
+numeros.splice (3,2)
+console.log (numeros) /* numero 4 e 5 removido 
+
+console.log(numeros.length) /* novo tamanho array */ 
+
+
+
