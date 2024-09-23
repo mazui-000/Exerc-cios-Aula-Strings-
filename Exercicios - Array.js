dcdcc4188923b282386f9ea8889e8f91c70f3540
Gustavo Mazui <gustavopmazui18@gmail.com>
2024-09-23 11:52:03 -0300

    Add files via upload

---
+++ Exercicios - Array.js
@@ -0,0 +1,82 @@
+/////////////////////////////////////* Exercícios de interpretação de código 1
+
+/*let array
+console.log('a. ', array)
+/* sera impresso undefined no console. Por que não definimos valores a array" */
+
+/*array = null
+console.log('b. ', array)
+/* Será impresso que a array foi definida como null*/
+
+
+/*array = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+console.log('c. ', array.length)
+/* Será Impresso que foi definido, e mostra os 11 elementos da array*
+
+let i = 0
+console.log('d. ', array[i])
+/* Será Impresso a posição 0 da array, o primeiro numero (3)*/ 
+
+/*array[i+1] = 19
+console.log('e. ', array)
+/* Será impresso no console a array com o numero 4, substituido por 19 */ 
+
+/*const valor = array[i+6]
+console.log('f. ', valor)
+/* Será Impresso a soma da primeira posição com o valor 6 da array (3+6)*/ 
+
+
+////////////////////////////////////* Exercícios de interpretação de código 2 
+
+
+/* const frase = prompt("Digite uma frase")
+console.log(frase.toUpperCase().replaceAll("A", "I"), frase.length) 
+
+/* Será impresso a frase alterada na lingua do A e I. "SUBI NUM ÔNIBUS EM MIRROCOS 27" O numero 27 é o numero de carácteres, contando também os espaços */ 
+
+
+
+
+////////////////////////////////////* Exercícios de escrita de código 1
+
+/* let nome = prompt("Digite seu nome:")
+let email = prompt("Digite seu email:") 
+
+console.log(`O email ${email} foi cadastrado com sucesso. Seja Bem-vindo, ${nome}`)  */ 
+
+
+
+////////////////////////////////////* Exercícios de escrita de código 2 
+
+/*let comida = ["Pizza", "Pastel", "Xis", "churrasco", "pão de quei0jo"] 
+console.log(comida)
+
+console.log("Essas são as minhas comidas preferidas:")
+
+console.log(comida[0])
+console.log(comida[1]) 
+console.log(comida[2])  
+console.log(comida[3])    
+console.log(comida[4])    
+
+let comidaUsuario = prompt("Qual sua comida preferida?")
+comida[1] = comidaUsuario
+
+console.log(comida)*/
+
+
+////////////////////////////////////* Exercícios de escrita de código 3
+
+/*let array 
+let listadeTarefas = Array
+const tarefa1 = prompt ("Insire uma tarefa que você pratica no dia a dia")
+const tarefa2 = prompt ("Segunda Tarefa")
+const tarefa3 = prompt ("Terceira Tarefa")
+
+array = [tarefa1, tarefa2, tarefa3]
+console.log ("Lista de Tarefas:", (array) )  
+
+const indice = prompt ("Digite o indice de uma tarefa que voce já realizou")
+
+array.splice(indice, 1) 
+console.log (array)*/ 
